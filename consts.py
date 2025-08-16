cdedc87922f632142ca04139cd6fe01ef7cb2709
Pincopallo92 <156342132+Pincopallo92@users.noreply.github.com>
2025-08-16 13:26:24 +0200

    consts.py

---
+++ consts.py
@@ -0,0 +1,19 @@
+# consts.py
+TOKEN = "7639507455:AAFxqE-xEc7MxBY0MzhH2PGQ01_pvs0QPl4"
+CHANNEL_NAME = "lowpriceamazonitaly"
+NUMBER_OF_MESSAGES = 2
+MIN_HOUR = 8
+MAX_HOUR = 22
+PAUSE_MINUTES = 30
+
+# Le categorie di ricerca Amazon
+CATEGORIES = {
+    "libri": ["python", "algoritmi", "fantascienza"],
+    "elettronica": ["cuffie", "mouse", "tastiera"]
+}
+
+# Chiavi Amazon
+AMAZON_ACCESS_KEY = "AKPADTHF2X1755088506"
+AMAZON_SECRET_KEY = "0zn8eOoTu6rV1kWGUNixVGZvtpa603l4wjVkY2cn"
+AMAZON_ASSOCIATE_TAG = "enrigiarru-21"
+MAX_PAGE_SEARCH = 2
